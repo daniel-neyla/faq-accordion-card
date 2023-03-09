5fc95f7ccc8f51e7eef9f06d94f352a797646759
Daniel Neyla <daniel.neyla@gmail.com>
2023-03-09 19:04:00 +0000

    Done the javascript stuff

---
+++ script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+console.log("hello world");
+
+const questions = document.querySelectorAll(".faq__question");
+const arrowBtns = document.querySelectorAll(".faq__item-arrow-btn");
+
+let activeFaqItem = "";
+
+const toggleAnswer = function (faqItem) {
+  if (activeFaqItem === faqItem) {
+    faqItem.classList.remove("active");
+    activeFaqItem = "";
+  } else if (activeFaqItem) {
+    activeFaqItem.classList.remove("active");
+    faqItem.classList.add("active");
+    activeFaqItem = faqItem;
+  } else {
+    faqItem.classList.add("active");
+    activeFaqItem = faqItem;
+  }
+};
+
+arrowBtns.forEach((btn) => {
+  console.log(btn);
+  btn.addEventListener("click", () => {
+    const faqItem = btn.closest(".faq__faq-item");
+    toggleAnswer(faqItem);
+  });
+});
+
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const faqItem = question.closest(".faq__faq-item");
+    toggleAnswer(faqItem);
+  });
+});
